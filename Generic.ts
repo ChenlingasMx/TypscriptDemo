6c2873463f9fd3c6cd9fdad2e9ca0a8b1a644515
WX <42308734@qq.com>
2021-01-13 15:27:47 +0800

    fix:类的泛型

--- Generic.ts
+++ Generic.ts
@@ -29,3 +29,20 @@ function moreFun<T,P>(first:T,second:P){
   console.log(`${first}${second}`)
 } 
 moreFun<string,number>('1',1)
+
+/*类的泛型*/
+interface Girl {
+  name:string
+}
+class SelectGirl<T extends Girl>{
+  constructor(private girls:T[]){}
+  getGirl(index:number){
+    console.log(this.girls[index].name)
+  }
+}
+const newGirls = new SelectGirl([
+  { name: "大脚" },
+  { name: "刘英" },
+  { name: "晓红" },
+])
+newGirls.getGirl(1)
