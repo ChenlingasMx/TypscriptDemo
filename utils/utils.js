852651dc8fdbb778b2f93a375f8c9a01103dcff0
WX <42308734@qq.com>
2021-02-12 09:45:46 +0800

    fix:数组去重

--- utils/utils.js
+++ utils/utils.js
@@ -126,3 +126,14 @@ export function promiseAll(promises) {
       }
   })
 }
+
+// 数组去重
+function unique(arr){
+  var newArr = [];
+  for(var i = 0; i < arr.length; i++){
+      if(newArr.indexOf(arr[i]) == -1){
+          newArr.push(arr[i])
+      }
+  }
+  return newArr;
+} 
